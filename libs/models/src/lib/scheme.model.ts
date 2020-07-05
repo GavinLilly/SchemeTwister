034fad647c689c2d44ac610a8240ea6d5d193501
Gavin Lilly <gavtherav@gmail.com>
2020-07-05 22:53:34 +0100

    Added missing rules definitions to prevent rules being missing from schemes

--- libs/models/src/lib/scheme.model.ts
+++ libs/models/src/lib/scheme.model.ts
@@ -33,5 +33,36 @@ export class SchemeModel extends BaseModel implements IScheme {
   requiredVillainsID: string;
 
   @Type(() => RulesModel)
-  rules: RulesModel;
+  rules: RulesModel = {
+    numVillains: {
+      2: 2,
+      3: 3,
+      4: 3,
+      5: 4,
+    },
+    numHenchmen: {
+      2: 1,
+      3: 1,
+      4: 2,
+      5: 2,
+    },
+    numBystanders: {
+      2: 2,
+      3: 8,
+      4: 8,
+      5: 12,
+    },
+    numHeroes: {
+      2: 5,
+      3: 5,
+      4: 5,
+      5: 6,
+    },
+    numTwists: {
+      2: 8,
+      3: 8,
+      4: 8,
+      5: 8,
+    },
+  };
 }
