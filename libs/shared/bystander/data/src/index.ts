766b9bedbcfe5afeacde7c24cade6038c8a903f4
Gavin Lilly <gavtherav@gmail.com>
2020-07-20 08:56:09 +0100

    refactor(lib): update bystander to use new definition format

--- libs/shared/bystander/data/src/index.ts
+++ libs/shared/bystander/data/src/index.ts
@@ -1,1 +1,8 @@
-export * from './lib/legendary';
+import { LegendaryBystanders } from './lib/legendary';
+import { BystanderModel } from '@legendizer/shared/bystander/types';
+
+export { LegendaryBystanders };
+
+export const ALL_BYSTANDERS: BystanderModel[] = [
+  ...LegendaryBystanders.values(),
+];
--- libs/shared/bystander/data/src/lib/legendary.ts
+++ libs/shared/bystander/data/src/lib/legendary.ts
@@ -1,14 +1,14 @@
-import { Bystander } from '@legendizer/shared/bystander/types';
-import { gameSets } from '@legendizer/shared/gameSet/data';
+import { BystanderModel } from '@legendizer/shared/bystander/types';
+import { GameSet } from '@legendizer/shared/gameSet/data';
+import { CardType } from "@legendizer/shared/base/types";
 
-export const bystanders: Bystander[] = [
-  {
+export class LegendaryBystanders extends BystanderModel {
+  public static BYSTANDER = new LegendaryBystanders({
     id: 'd84ef365-e212-430b-9925-c567ef17569f',
     name: 'Bystander',
     copies: 30,
     victoryPoints: 1,
-    gameSet: gameSets.find(
-      (record) => record.id === 'aa4eb824-5316-4f0d-bca7-a072b58dee5d'
-    ),
-  },
-];
+    gameSet: GameSet.LEGENDARY,
+    cardType: CardType.BYSTANDER
+  })
+}
