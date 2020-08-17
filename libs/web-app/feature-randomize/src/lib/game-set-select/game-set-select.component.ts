62ed89cb95b776e6c26654a531818ae5ead0d9a7
Gavin Lilly <gavtherav@gmail.com>
2020-08-17 22:36:06 +0100

    feat(app): store game set selection in a cookie

--- libs/web-app/feature-randomize/src/lib/game-set-select/game-set-select.component.ts
+++ libs/web-app/feature-randomize/src/lib/game-set-select/game-set-select.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { GameSets, IGameSet } from "@legendizer/legendizer-lib";
+import { GameSets, IGameSet } from '@legendizer/legendizer-lib';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { GameSetupStore } from '../game-setup-store';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'legendizer-game-set-select',
   templateUrl: './game-set-select.component.html',
-  styleUrls: ['./game-set-select.component.scss']
+  styleUrls: ['./game-set-select.component.scss'],
 })
 export class GameSetSelectComponent implements OnInit {
-
+  private COOKIE_NAME = 'SelectedGameSets';
   selectedGameSets: IGameSet[];
   gameSets: IGameSet[] = GameSets.ALL;
 
-  constructor(public gameSetupStore: GameSetupStore, public activeModal: NgbActiveModal) { }
+  constructor(
+    public gameSetupStore: GameSetupStore,
+    public activeModal: NgbActiveModal,
+    private cookieService: CookieService
+  ) {}
 
   ngOnInit(): void {
-    this.gameSetupStore.gameSets.subscribe((next) => {
-      this.selectedGameSets = next;
-    });
+    this.selectedGameSets = this.cookieService.check(this.COOKIE_NAME)
+      ? this.cookieToArray(this.cookieService.get(this.COOKIE_NAME))
+      : GameSets.ALL
+
+    this.setGameSets();
+  }
+
+  private cookieToArray(idString: string): IGameSet[] {
+    const idArray: string[] = idString.split('|');
+    return GameSets.ALL.filter((item) => idArray.includes(item.id))
   }
 
   setGameSets() {
-    this.gameSetupStore.setGameSets(this.selectedGameSets)
+    console.log(this.selectedGameSets.map((item) => item.id))
+    this.cookieService.set(this.COOKIE_NAME, this.selectedGameSets.map((item) => item.id).join('|'));
+    this.gameSetupStore.setGameSets(this.selectedGameSets);
   }
-
 }
