12f7837b15856b8f6fb80a32ded09179695cb9b5
Gavin Lilly <gavtherav@gmail.com>
2020-08-25 21:47:28 +0100

    feat(legendizer-lib): add Fantastic Four villains

--- libs/legendizer-lib/src/lib/villains/villainGroups.ts
+++ libs/legendizer-lib/src/lib/villains/villainGroups.ts
@@ -1,6 +1,7 @@
 import { CardSet } from '../cardSet';
 import { AntMan } from './antMan.villains';
 import { DarkCity } from './darkCity.villains';
+import { FantasticFour } from './fantasticFour.villains';
 import { GuardiansOfTheGalaxy } from './guardiansOfTheGalaxy.villains';
 import { HeroesOfAsgard } from "./heroesOfAsgard.villains";
 import { Legendary } from './legendary.villains';
@@ -12,12 +13,14 @@ export class VillainGroups extends CardSet<IVillainGroup> {
   public static readonly HEROES_OF_ASGARD = HeroesOfAsgard
   public static readonly GUARDIANS_OF_THE_GALAXY = GuardiansOfTheGalaxy
   public static readonly ANT_MAN = AntMan;
+  public static readonly FANTASTIC_FOUR = FantasticFour;
 
   public static readonly ALL = [
     ...Object.values(VillainGroups.LEGENDARY),
     ...Object.values(VillainGroups.DARK_CITY),
     ...Object.values(VillainGroups.HEROES_OF_ASGARD),
     ...Object.values(VillainGroups.GUARDIANS_OF_THE_GALAXY),
-    ...Object.values(VillainGroups.ANT_MAN)
+    ...Object.values(VillainGroups.ANT_MAN),
+    ...Object.values(VillainGroups.FANTASTIC_FOUR)
   ];
 }
