c2b5ab1014a24d6cb6e4f69dc6e79f0ff8ace4c5
Gavin Lilly <gavtherav@gmail.com>
2020-08-25 01:25:25 +0100

    feat(legendizer-lib): add keywords for Ant-Man

--- libs/legendizer-lib/src/lib/cardSet/keyword.enum.ts
+++ libs/legendizer-lib/src/lib/cardSet/keyword.enum.ts
@@ -1,7 +1,11 @@
 export enum Keyword {
   ARTIFACTS = "Artifacts",
   BRIBE = "Bribe",
+  CHIVALROUS_DUEL = "Chivalrous Duel",
   CONQUEROR = "Conqueror",
+  EMPOWERED = "Empowered",
+  MICROSCOPIC_SIZE_CHANGING = "Microscopic Size-Changing",
+  SIZE_CHANGING = "Size-Changing",
   TELEPORT = "Teleport",
   THROWN_ARTIFACTS = "Thrown Artifacts",
   VERSATILE = "Versatile",
