712690923264a970d340eb146c5561016d0a9a39
Gavin Lilly <gavtherav@gmail.com>
2020-08-27 19:51:52 +0100

    test(app): manual mock of CookieService

--- libs/web-app/feature-randomize/src/lib/game-setup-store.spec.ts
+++ libs/web-app/feature-randomize/src/lib/game-setup-store.spec.ts
@@ -1,7 +1,21 @@
+import { CookieService } from 'ngx-cookie-service';
+
 import { GameSetupStore } from './game-setup-store';
 
 describe('GameSetupStore', () => {
+  const _cookieReg = {};
+  const fakeCookie = {
+    check: (name: string): boolean => {
+      return _cookieReg[name] ? true : false;
+    },
+    get: (name: string): string => {
+      return _cookieReg[name];
+    },
+    set: (name: string, value: string) => {
+      _cookieReg[name] = name + '=' + value;
+    }
+  };
   it('should create an instance', () => {
-    expect(new GameSetupStore()).toBeTruthy();
+    expect(new GameSetupStore(fakeCookie as CookieService)).toBeTruthy();
   });
 });
