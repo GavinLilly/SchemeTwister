49480bcddbb97931a244ce84092b218f01a69f3a
Gavin Lilly <gavtherav@gmail.com>
2020-08-28 22:38:36 +0100

    feat(legendizer-lib): add Paint the Town Red keywords

--- libs/legendizer-lib/src/lib/cardSet/keyword.enum.ts
+++ libs/legendizer-lib/src/lib/cardSet/keyword.enum.ts
@@ -4,11 +4,13 @@ export enum Keyword {
   CHIVALROUS_DUEL = "Chivalrous Duel",
   CONQUEROR = "Conqueror",
   EMPOWERED = "Empowered",
+  FEAST = "Feast",
   MICROSCOPIC_SIZE_CHANGING = "Microscopic Size-Changing",
   SIZE_CHANGING = "Size-Changing",
   TELEPORT = "Teleport",
   THROWN_ARTIFACTS = "Thrown Artifacts",
   VERSATILE = "Versatile",
   VILLAINOUS_WEAPONS = "Villainous Weapons",
+  WALL_CRAWL = "Wall-Crawl",
   WORTHY = "Worthy"
 }
