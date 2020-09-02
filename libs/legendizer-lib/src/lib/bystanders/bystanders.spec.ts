54b150ef80cc6c4d057f7ededcfd832c3f74b84e
Gavin Lilly <gavtherav@gmail.com>
2020-09-02 15:39:51 +0100

    feat(legendizer-lib): add X-Men bystanders

--- libs/legendizer-lib/src/lib/bystanders/bystanders.spec.ts
+++ libs/legendizer-lib/src/lib/bystanders/bystanders.spec.ts
@@ -1,3 +1,5 @@
+import { CardType } from '../cardSet';
+import { IHero } from '../heroes';
 import { Bystanders } from './bystanders';
 
 describe('Legendary base set', () => {
@@ -8,4 +10,13 @@ describe('Legendary base set', () => {
 describe('Dark City set', () => {
   it('should have 3 entries', () =>
     expect(Object.values(Bystanders.DARK_CITY)).toHaveLength(3));
+});
+
+describe('X-Men set', () => {
+  const xMenBystanders: IHero[] = Object.values(Bystanders.X_MEN);
+  it('should have 10 entries', () => expect(xMenBystanders).toHaveLength(10));
+  it('should only contain bystander type cards', () =>
+    expect(
+      xMenBystanders.every((item) => item.cardType === CardType.BYSTANDER)
+    ).toBeTruthy());
 });
