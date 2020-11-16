9b97c1fe18adbe39aa48aa89a52c686aa0083f06
Gavin Lilly <gavtherav@gmail.com>
2020-11-16 11:40:34 +0000

    build(*): merge commitlint and commitizen configs with VS Code

---
+++ .cz-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    {
+      value: 'style',
+      name:
+        'style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: 'refactor',
+      name:
+        'refactor: A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance',
+    },
+    {
+      value: 'test',
+      name: 'test:     Adding missing tests or correcting existing tests',
+    },
+    {
+      value: 'build',
+      name:
+        'build:    Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+    },
+    {
+      value: 'ci',
+      name:
+        'ci:       Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+    },
+    {
+      value: 'chore',
+      name:
+        'chore:    Changes to the build process or auxiliary tools and libraries such as documentation generation',
+    },
+    { value: 'revert', name: 'revert:   Revert to a commit' },
+  ],
+  scopes: [
+    { name: 'docs' },
+    { name: 'app' },
+    { name: 'api' },
+    { name: 'misc' },
+    { name: 'legendizer-lib' },
+    { name: 'deps' },
+    { name: '*' },
+  ],
+  subjectLimit: 72,
+};
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [2, 'always', [
-      'docs',
-      'app',
-      'api',
-      'misc',
-      'legendizer-lib',
-      'deps',
-      '*'
-    ]]
-  }
+  extends: ['@commitlint/config-conventional', 'cz'],
 };
