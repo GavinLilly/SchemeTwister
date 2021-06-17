ef879a4b5c591b8eb4f2a0f1e22da494ddac9af8
Gavin Lilly <gavtherav@gmail.com>
2021-06-17 23:29:38 +0100

    chore(*): update Angular and NX to v12

--- apps/web-api/jest.config.js
+++ apps/web-api/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   coverageDirectory: '../../coverage/apps/web-api',
   globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
   displayName: 'web-api',
+  testEnvironment: 'node',
 };
