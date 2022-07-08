9b4a549fdb4ea56ca845e9885aebb75742e377d4
Gavin Lilly <gavtherav@gmail.com>
2022-07-08 21:56:07 +0100

    test(app): allow tests to pass

--- libs/web-app/feature-randomize/src/lib/+state/selectors/game-sets.selectors.spec.ts
+++ libs/web-app/feature-randomize/src/lib/+state/selectors/game-sets.selectors.spec.ts
@@ -1,7 +1,25 @@
+import { GameSets } from '@schemetwister/libtwister';
 
+import { IGameSetsState } from '../reducers/game-sets.reducer';
+
+import { selectGameSetIds, selectGameSets } from './game-sets.selectors';
 
 describe('GameSets Selectors', () => {
-  it('should select the feature state', () => {
-    
+  const initialState: IGameSetsState = {
+    gameSetIds: [GameSets.LEGENDARY.default.id, GameSets.DARK_CITY.default.id],
+    loading: false,
+    error: '',
+  };
+
+  it('should select the IDs', () => {
+    const result = selectGameSetIds.projector(initialState);
+    expect(result).toHaveLength(2);
+    expect(result).toContain(GameSets.LEGENDARY.default.id);
+  });
+
+  it('should select the GameSets', () => {
+    const result = selectGameSets.projector(initialState);
+    expect(result).toHaveLength(2);
+    expect(result).toContain(GameSets.LEGENDARY.default);
   });
 });
