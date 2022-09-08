9eea955d8e63f18e38b1727212b0b032f59dd6b5
Gavin Lilly <gavtherav@gmail.com>
2022-09-08 08:49:21 +0100

    feat(libtwister): add Black Widow expansion

---
+++ libs/libtwister/src/lib/data/gameSets/blackWidow/keywords.ts
@@ -0,0 +1,8 @@
+import { IKeyword } from '../../../model';
+import { KeywordName } from '../../enums';
+
+export const UNLEASH: IKeyword = {
+  name: KeywordName.UNLEASH,
+  id: 'e28fc7dd-6173-4a89-9459-c3aa1314a288',
+  description: 'TBD',
+};
