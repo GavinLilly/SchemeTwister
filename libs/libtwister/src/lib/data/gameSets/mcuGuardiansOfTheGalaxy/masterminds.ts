d0b2bffb442934fce097110e42f1608dc43f6b36
Gavin Lilly <gavtherav@gmail.com>
2023-02-17 20:10:12 +0000

    refactor(libtwister): reduce duplicate code for MCU GOG masterminds

--- libs/libtwister/src/lib/data/gameSets/mcuGuardiansOfTheGalaxy/masterminds.ts
+++ libs/libtwister/src/lib/data/gameSets/mcuGuardiansOfTheGalaxy/masterminds.ts
@@ -5,48 +5,53 @@ import { TRIGGERED_ARTIFACTS } from './keywords';
 import { META } from './meta';
 import { FOLLOWERS_OF_RONAN } from './villains';
 
-class McuGuardiansOfTheGalaxy extends AbstractMastermind {
+abstract class AbstractMcuGuardiansOfTheGalaxyMastermind extends AbstractMastermind {
   public readonly gameSetId = META.id;
 }
 
-export const RONAN_THE_ACCUSER = new McuGuardiansOfTheGalaxy(
+class RonanTheAccuser extends AbstractMcuGuardiansOfTheGalaxyMastermind {
+  constructor(id: string, attackPoints: number, epic = false) {
+    super(
+      id,
+      'Ronan the Accuser',
+      attackPoints,
+      0,
+      FOLLOWERS_OF_RONAN,
+      epic,
+      VILLAINOUS_WEAPONS,
+      TRIGGERED_ARTIFACTS
+    );
+  }
+}
+
+export const RONAN_THE_ACCUSER = new RonanTheAccuser(
   '1faead13-40f8-4583-8aa1-9b16778aa1d7',
-  'Ronan the Accuser',
-  6,
-  0,
-  FOLLOWERS_OF_RONAN,
-  false,
-  VILLAINOUS_WEAPONS,
-  TRIGGERED_ARTIFACTS
+  6
 );
 
-export const EPIC_RONAN_THE_ACCUSER = new McuGuardiansOfTheGalaxy(
+export const EPIC_RONAN_THE_ACCUSER = new RonanTheAccuser(
   'f2defecb-0e5c-4b4a-9d5c-cb17f707a744',
-  'Ronan the Accuser',
   7,
-  0,
-  FOLLOWERS_OF_RONAN,
-  true,
-  VILLAINOUS_WEAPONS,
-  TRIGGERED_ARTIFACTS
+  true
 );
 
-export const EGO_THE_LIVING_PLANET = new McuGuardiansOfTheGalaxy(
+class EgoTheLivingPlanet extends AbstractMcuGuardiansOfTheGalaxyMastermind {
+  constructor(id: string, attackPoints: string, epic = false) {
+    super(id, 'Ego, the Living Planet', attackPoints, 0, undefined, epic);
+  }
+}
+
+export const EGO_THE_LIVING_PLANET = new EgoTheLivingPlanet(
   'b901a133-3a12-4492-9efb-a9dcea7c53b6',
-  'Ego, The Living Planet',
-  '3+',
-  0
+  '3+'
 ).withRuleOverride((rule) => {
   rule.villainDeck.numVillainGroups++;
   return rule;
 });
 
-export const EPIC_EGO_THE_LIVING_PLANET = new McuGuardiansOfTheGalaxy(
+export const EPIC_EGO_THE_LIVING_PLANET = new EgoTheLivingPlanet(
   '7474eef1-38f4-4127-8ce8-e38296fadcc1',
-  'Ego, The Living Planet',
   '1+',
-  0,
-  undefined,
   true
 ).withRuleOverride((rule) => {
   rule.villainDeck.numVillainGroups += 2;
