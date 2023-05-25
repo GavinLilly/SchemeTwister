39bef9f85afd7409d5791d7e483c35b4599b063d
Gavin Lilly <gavtherav@gmail.com>
2023-05-25 01:57:44 +0100

    test(libtwister): add test for GameSetMap

---
+++ libtwister/src/lib/model/gameSetMap.spec.ts
@@ -0,0 +1,32 @@
+import { LibTwister } from '../libTwister';
+
+import { GameSetMap } from './gameSetMap';
+import { GameSetSize } from './gameSetSize.enum';
+import { Series } from './series.enum';
+
+describe('GameSetMap', () => {
+  let map: GameSetMap;
+
+  beforeAll(() => {
+    map = LibTwister.allGameSets;
+  });
+
+  describe('asArray', () => {
+    it('should have all game sets', () =>
+      expect(map.asArray()).toHaveLength(34));
+
+    it('should only have core game sets', () => {
+      const sizes = new Set(
+        map.asArray({ size: GameSetSize.CORE }).map((gameset) => gameset.size)
+      );
+      expect(sizes.size).toEqual(1);
+    });
+
+    it('should only have MCU game sets', () => {
+      const sizes = new Set(
+        map.asArray({ series: Series.MCU }).map((gameset) => gameset.series)
+      );
+      expect(sizes.size).toEqual(1);
+    });
+  });
+});
