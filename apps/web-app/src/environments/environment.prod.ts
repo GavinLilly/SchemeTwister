be574f95f438d8d333292c1da2d94fd647d8f156
Gavin Lilly <gavtherav@gmail.com>
2023-06-21 00:43:50 +0100

    ci(app): use github secrets for firebase

--- apps/web-app/src/environments/environment.prod.ts
+++ apps/web-app/src/environments/environment.prod.ts
@@ -3,13 +3,13 @@ import { FirebaseOptions } from '@angular/fire';
 export const environment = {
   production: true,
   firebase: {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: ${FIREBASE_API_KEY},
+    authDomain: ${FIREBASE_AUTH_DOMAIN},
+    projectId: ${FIREBASE_PROJECT_ID},
+    storageBucket: ${FIREBASE_STORAGE_BUCKET},
+    messagingSenderId: ${FIREBASE_MESSAGING_SENDER},
+    appId: ${FIREBASE_APP_ID},
+    measurementId: ${FIREBASE_MEASUREMENT_ID},
   } as FirebaseOptions,
   firestoreCollection: 'setups',
 };
