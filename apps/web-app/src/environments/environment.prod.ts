33499df16d1750402de12a5c1d077f747003dea8
Gavin Lilly <gavtherav@gmail.com>
2023-06-21 00:46:59 +0100

    ci(app): fix wrong messaging sender token

--- apps/web-app/src/environments/environment.prod.ts
+++ apps/web-app/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     authDomain: ${FIREBASE_AUTH_DOMAIN},
     projectId: ${FIREBASE_PROJECT_ID},
     storageBucket: ${FIREBASE_STORAGE_BUCKET},
-    messagingSenderId: ${FIREBASE_MESSAGING_SENDER},
+    messagingSenderId: ${FIREBASE_MESSAGING_SENDER_ID},
     appId: ${FIREBASE_APP_ID},
     measurementId: ${FIREBASE_MEASUREMENT_ID},
   } as FirebaseOptions,
