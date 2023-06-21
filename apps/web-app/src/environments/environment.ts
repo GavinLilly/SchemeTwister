95ca5907d9fefef0c2312e93a52cdf712c01fc4c
Gavin Lilly <gavtherav@gmail.com>
2023-06-21 10:43:05 +0100

    ci(app): wrap tokens with quotes

--- apps/web-app/src/environments/environment.ts
+++ apps/web-app/src/environments/environment.ts
@@ -7,13 +7,13 @@ import { FirebaseOptions } from '@angular/fire';
 export const environment = {
   production: false,
   firebase: {
-    apiKey: ${FIREBASE_API_KEY},
-    authDomain: ${FIREBASE_AUTH_DOMAIN},
-    projectId: ${FIREBASE_PROJECT_ID},
-    storageBucket: ${FIREBASE_STORAGE_BUCKET},
-    messagingSenderId: ${FIREBASE_MESSAGING_SENDER_ID},
-    appId: ${FIREBASE_APP_ID},
-    measurementId: ${FIREBASE_MEASUREMENT_ID},
+    apiKey: '${FIREBASE_API_KEY}',
+    authDomain: '${FIREBASE_AUTH_DOMAIN}',
+    projectId: '${FIREBASE_PROJECT_ID}',
+    storageBucket: '${FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${FIREBASE_APP_ID}',
+    measurementId: '${FIREBASE_MEASUREMENT_ID}',
   } as FirebaseOptions,
   firestoreCollection: 'setups-test',
 };
