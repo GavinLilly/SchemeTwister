2064c69f6be737129500f8792c493561ff10f9eb
Gavin Lilly <gavtherav@gmail.com>
2023-08-31 01:19:59 +0100

    refactor(*): move team icons to libtwister

--- libs/libtwister/src/lib/data/teams/teams.ts
+++ libs/libtwister/src/lib/data/teams/teams.ts
@@ -2,120 +2,120 @@ import { ITeam } from '../../model';
 
 export const AVENGERS: ITeam = {
   name: 'Avengers',
-  icon: '/assets/icons/teams/avengers.png',
+  icon: 'avengers.png',
 };
 
 export const FANTASTIC_FOUR: ITeam = {
   name: 'Fantastic Four',
-  icon: '/assets/icons/teams/fantastic-four.png',
+  icon: 'fantastic-four.png',
 };
 
 export const GUARDIANS_OF_THE_GALAXY: ITeam = {
   name: 'Guardians of the Galaxy',
-  icon: '/assets/icons/teams/guardians.png',
+  icon: 'guardians.png',
 };
 
 export const HEROES_OF_ASGARD: ITeam = {
   name: 'Heroes of Asgard',
-  icon: '/assets/icons/teams/heroes-of-asgard.png',
+  icon: 'heroes-of-asgard.png',
 };
 
 export const HEROES_OF_WAKANDA: ITeam = {
   name: 'Heroes of Wakanda',
-  icon: '/assets/icons/teams/heroes-of-wakanda.png',
+  icon: 'heroes-of-wakanda.png',
 };
 
 export const MARVEL_KNIGHTS: ITeam = {
   name: 'Marvel Knights',
-  icon: '/assets/icons/teams/marvel-knights.png',
+  icon: 'marvel-knights.png',
 };
 
 export const SHIELD: ITeam = {
   name: 'S.H.I.E.L.D.',
-  icon: '/assets/icons/teams/shield.png',
+  icon: 'shield.png',
 };
 
 export const SPIDER_FRIENDS: ITeam = {
   name: 'Spider Friends',
-  icon: '/assets/icons/teams/spider-friends.png',
+  icon: 'spider-friends.png',
 };
 
 export const X_FORCE: ITeam = {
   name: 'X-Force',
-  icon: '/assets/icons/teams/x-force.png',
+  icon: 'x-force.png',
 };
 
 export const X_MEN: ITeam = {
   name: 'X-Men',
-  icon: '/assets/icons/teams/x-men.png',
+  icon: 'x-men.png',
 };
 
 export const BROTHERHOOD: ITeam = {
   name: 'Brotherhood',
-  icon: '/assets/icons/teams/brotherhood.png',
+  icon: 'brotherhood.png',
 };
 
 export const CABAL: ITeam = {
   name: 'Cabal',
-  icon: '/assets/icons/teams/cabal.png',
+  icon: 'cabal.png',
 };
 
 export const CHAMPIONS: ITeam = {
   name: 'Champions',
-  icon: '/assets/icons/teams/champions.png',
+  icon: 'champions.png',
 };
 
 export const CRIME_SYNDICATE: ITeam = {
   name: 'Crime Syndicate',
-  icon: '/assets/icons/teams/crime-syndicate.png',
+  icon: 'crime-syndicate.png',
 };
 
 export const FOES_OF_ASGARD: ITeam = {
   name: 'Foes of Asgard',
-  icon: '/assets/icons/teams/foes-of-asgard.png',
+  icon: 'foes-of-asgard.png',
 };
 
 export const HYDRA: ITeam = {
   name: 'Hydra',
-  icon: '/assets/icons/teams/hydra.png',
+  icon: 'hydra.png',
 };
 
 export const ILLUMINATI: ITeam = {
   name: 'Illuminati',
-  icon: '/assets/icons/teams/illuminati.png',
+  icon: 'illuminati.png',
 };
 
 export const INHUMANS: ITeam = {
   name: 'Inhumans',
-  icon: '/assets/icons/teams/inhumans.png',
+  icon: 'inhumans.png',
 };
 
 export const MERCS_FOR_MONEY: ITeam = {
   name: 'Mercs for Money',
-  icon: '/assets/icons/teams/mercs-for-money.png',
+  icon: 'mercs-for-money.png',
 };
 
 export const NEW_WARRIORS: ITeam = {
   name: 'New Warriors',
-  icon: '/assets/icons/teams/new-warriors.png',
+  icon: 'new-warriors.png',
 };
 
 export const SINISTER_SIX: ITeam = {
   name: 'Sinister Six',
-  icon: '/assets/icons/teams/sinister-six.png',
+  icon: 'sinister-six.png',
 };
 
 export const VENOMVERSE: ITeam = {
   name: 'Venomverse',
-  icon: '/assets/icons/teams/venomverse.png',
+  icon: 'venomverse.png',
 };
 
 export const WARBOUND: ITeam = {
   name: 'Warbound',
-  icon: '/assets/icons/teams/warbound.png',
+  icon: 'warbound.png',
 };
 
 export const X_FACTOR: ITeam = {
   name: 'X-Factor',
-  icon: '/assets/icons/teams/x-factor.png',
+  icon: 'x-factor.png',
 };
