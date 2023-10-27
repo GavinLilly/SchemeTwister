641a94ef2b105019e57722460a8209c7f971aea1
Gavin Lilly <gavtherav@gmail.com>
2023-10-27 10:42:01 +0100

    style(*): fix linting issues

--- libs/web-app/feature-randomize/src/test-setup.ts
+++ libs/web-app/feature-randomize/src/test-setup.ts
@@ -1,13 +1,12 @@
 import 'jest-preset-angular/setup-jest';
-import { ngMocks } from 'ng-mocks';
-
-ngMocks.autoSpy('jest');
-
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import { ngMocks } from 'ng-mocks';
+
+ngMocks.autoSpy('jest');
 
 getTestBed().resetTestEnvironment();
 getTestBed().initTestEnvironment(
