1d3fc45322425ee9b83e7d3c851aa15e9d616127
Gavin Lilly <gavtherav@gmail.com>
2024-05-06 12:01:18 +0100

    refactor(app): remove unused duplicate code

--- libs/web-app/feature-randomize/src/lib/additional-deck/additional-deck.component.ts
+++ libs/web-app/feature-randomize/src/lib/additional-deck/additional-deck.component.ts
@@ -1,21 +1,13 @@
 import { Component, Signal } from '@angular/core';
 import { faLock, faLockOpen } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
-import {
-  Henchmen,
-  Hero,
-  IAdditionalDeck,
-  Mastermind,
-  VillainGroup,
-} from '@schemetwister/libtwister';
+import { IAdditionalDeck } from '@schemetwister/libtwister';
 
-import { additionalDeckActions } from '../+state/actions/game-setup.actions';
 import { IGameSetupState } from '../+state/reducers/game-setup.reducer';
 import {
   selectAdditionalDecks,
   selectLockedAdditionalDeckCards,
 } from '../+state/selectors/game-setup-scheme.selectors';
-import { VillainAdditionalDeckCardType } from '../villainAdditionalDeckCard.type';
 
 @Component({
   selector: 'schemetwister-additional-deck',
@@ -36,33 +28,4 @@ export class AdditionalDeckComponent {
       gameSetup: IGameSetupState;
     }>
   ) {}
-
-  isCardLocked(card: VillainAdditionalDeckCardType) {
-    const lockedCards = this.lockedCards();
-    if (lockedCards !== undefined) {
-      if (card instanceof Hero) {
-        return lockedCards.heroes?.includes(card);
-      }
-
-      if (card instanceof Henchmen) {
-        return lockedCards.henchmen?.includes(card);
-      }
-
-      if (card instanceof VillainGroup) {
-        return lockedCards.villains?.includes(card);
-      }
-
-      if (card instanceof Mastermind) {
-        return lockedCards.masterminds?.includes(card);
-      }
-    }
-
-    return false;
-  }
-
-  dispatchLock = (card: VillainAdditionalDeckCardType) =>
-    this._store.dispatch(additionalDeckActions.addCard({ card }));
-
-  dispatchReset = (card: VillainAdditionalDeckCardType) =>
-    this._store.dispatch(additionalDeckActions.removeCard({ card }));
 }
