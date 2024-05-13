5dcca4ab80f1be19ea1b41ce56167a1df7eff0a0
Gavin Lilly <gavtherav@gmail.com>
2024-05-13 23:46:47 +0100

    chore(*): add base vite config

--- libs/schemetwister-series-marvelstudios/vite.config.ts
+++ libs/schemetwister-series-marvelstudios/vite.config.ts
@@ -1,30 +1,17 @@
 /// <reference types='vitest' />
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineConfig } from 'vite';
+import baseConfig from '../../vite.base.config';
 
 export default defineConfig({
+  ...baseConfig,
   root: __dirname,
   cacheDir: '../../node_modules/.vite/libs/schemetwister-series-marvelstudios',
-
-  plugins: [nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
   test: {
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
+    ...baseConfig.test,
     coverage: {
+      ...baseConfig.test?.coverage,
       reportsDirectory:
         '../../coverage/libs/schemetwister-series-marvelstudios',
-      provider: 'v8',
     },
   },
 });
