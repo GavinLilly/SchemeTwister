e4ee3b1eab39b5b678c5e71492a64e75c4d7e135
Gavin Lilly <gavtherav@gmail.com>
2024-06-15 00:17:30 +0100

    test(libtwister): allow tests to work with modular firestore

--- libs/web-app/feature-view/src/lib/latest-setups.store.spec.ts
+++ libs/web-app/feature-view/src/lib/latest-setups.store.spec.ts
@@ -4,7 +4,6 @@ import {
   WebAppSharedModule,
 } from '@schemetwister/web-app/shared';
 import { MockBuilder, MockRender } from 'ng-mocks';
-import { of } from 'rxjs';
 
 import { LatestSetupsStore } from './latest-setups.store';
 import { WebAppFeatureViewModule } from './web-app-feature-view.module';
@@ -15,7 +14,7 @@ describe('LatestSetupsStore', () => {
       WebAppFeatureViewModule,
       WebAppSharedModule,
     ]).mock(StoredSetupsService, {
-      getLatestSetups: () => of([EMPTY_STORED_SETUP]),
+      getLatestSetups: () => Promise.resolve([EMPTY_STORED_SETUP]),
     })
   );
 
