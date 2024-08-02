e915ded2e33aef13d933bafb674dd646053cd3cc
Gavin Lilly <gavtherav@gmail.com>
2024-08-02 01:07:14 +0100

    test(app): test for screen lock state in storage

--- libs/web-app/feature-randomize/src/lib/screen-on-lock.store.spec.ts
+++ libs/web-app/feature-randomize/src/lib/screen-on-lock.store.spec.ts
@@ -4,33 +4,52 @@ import { LocalStorageService } from './local-storage.service';
 import { ScreenOnLockStore } from './screen-on-lock.store';
 
 describe('ScreenOnLockStore', () => {
-  let componentStore: ScreenOnLockStore;
+  describe('without any state in storage', () => {
+    let componentStore: ScreenOnLockStore;
 
-  beforeAll(async () => {
-    const localStorageService = new LocalStorageService<boolean>();
-    componentStore = new ScreenOnLockStore(localStorageService);
+    beforeAll(async () => {
+      const localStorageService = new LocalStorageService<boolean>();
+      componentStore = new ScreenOnLockStore(localStorageService);
+    });
+
+    it('should be created', () => {
+      expect(componentStore).toBeTruthy();
+    });
+
+    it("should show that the screen wake lock isn't on by default", () =>
+      expect(componentStore.state().isLocked).toEqual(false));
+
+    it('should toggle the screen wake lock', () => {
+      const takeOne = componentStore.state$.pipe(skip(1), take(1));
+
+      const trueSub = takeOne.subscribe(({ isLocked }) =>
+        expect(isLocked).toBe(true)
+      );
+      componentStore.toggleLock();
+      trueSub.unsubscribe();
+
+      const falseSub = takeOne.subscribe(({ isLocked }) =>
+        expect(isLocked).toBe(false)
+      );
+      componentStore.toggleLock();
+      falseSub.unsubscribe();
+    });
   });
 
-  it('should be created', () => {
-    expect(componentStore).toBeTruthy();
-  });
+  describe('with screen lock stored in localStorage', () => {
+    let componentStore: ScreenOnLockStore;
 
-  it("should show that the screen wake lock isn't on by default", () =>
-    expect(componentStore.state().isLocked).toEqual(false));
+    beforeAll(async () => {
+      const localStorageService = new LocalStorageService<boolean>();
+      localStorageService.set('isScreenLocked', true);
+      componentStore = new ScreenOnLockStore(localStorageService);
+    });
 
-  it('should toggle the screen wake lock', () => {
-    const takeOne = componentStore.state$.pipe(skip(1), take(1));
+    it('should be created', () => {
+      expect(componentStore).toBeTruthy();
+    });
 
-    const trueSub = takeOne.subscribe(({ isLocked }) =>
-      expect(isLocked).toBe(true)
-    );
-    componentStore.toggleLocked();
-    trueSub.unsubscribe();
-
-    const falseSub = takeOne.subscribe(({ isLocked }) =>
-      expect(isLocked).toBe(false)
-    );
-    componentStore.toggleLocked();
-    falseSub.unsubscribe();
+    it('should show that the screen wake lock is on by default', () =>
+      expect(componentStore.state().isLocked).toEqual(true));
   });
 });
