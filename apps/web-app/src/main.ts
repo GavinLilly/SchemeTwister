8413a1fb4c25c519faa0102a268658c260cf0e76
Gavin Lilly <gavtherav@gmail.com>
2024-08-20 00:16:39 +0100

    build(app): add firebase emulation for local dev

--- apps/web-app/src/main.ts
+++ apps/web-app/src/main.ts
@@ -3,8 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { EnvironmentType } from './environments/environmentType';
 
-if (environment.production) {
+if (environment.environmentType === EnvironmentType.PROD) {
   enableProdMode();
 }
 
