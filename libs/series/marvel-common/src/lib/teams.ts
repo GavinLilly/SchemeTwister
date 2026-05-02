3166282f7cbae77107e31a5bca633c945e467391
Gavin Lilly <gavtherav@gmail.com>
2026-05-02 22:55:34 +0100

    refactor(gamesets): move all series to inline their team icons

--- libs/series/marvel-common/src/lib/teams.ts
+++ libs/series/marvel-common/src/lib/teams.ts
@@ -1,31 +1,37 @@
 import { ITeam } from '@schemetwister/libtwister';
+import avengers from '../assets/icons/teams/avengers.png';
+import crimeSyndicate from '../assets/icons/teams/crime-syndicate.png';
+import guardiansGalaxy from '../assets/icons/teams/guardians-galaxy.png';
+import hydra from '../assets/icons/teams/hydra.png';
+import shield from '../assets/icons/teams/shield.png';
+import spiderFriends from '../assets/icons/teams/spider-friends.png';
 
 export const AVENGERS: ITeam = {
   name: 'Avengers',
-  icon: 'avengers.png',
+  icon: avengers,
 };
 
 export const CRIME_SYNDICATE: ITeam = {
   name: 'Crime Syndicate',
-  icon: 'crime-syndicate.png',
+  icon: crimeSyndicate,
 };
 
 export const GUARDIANS_OF_THE_GALAXY: ITeam = {
   name: 'Guardians of the Galaxy',
-  icon: 'guardians-galaxy.png',
+  icon: guardiansGalaxy,
 };
 
 export const HYDRA: ITeam = {
   name: 'Hydra',
-  icon: 'hydra.png',
+  icon: hydra,
 };
 
 export const SHIELD: ITeam = {
   name: 'S.H.I.E.L.D.',
-  icon: 'shield.png',
+  icon: shield,
 };
 
 export const SPIDER_FRIENDS: ITeam = {
   name: 'Spider Friends',
-  icon: 'spider-friends.png',
+  icon: spiderFriends,
 };
--- libs/series/marvel-studios/vite.config.ts
+++ libs/series/marvel-studios/vite.config.ts
@@ -1,13 +1,47 @@
 /// <reference types='vitest' />
 import { mergeConfig } from 'vite';
-
 import { sharedConfig } from '../../../vitest.shared';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default mergeConfig(sharedConfig, {
-  root: __dirname,
+  root: import.meta.dirname,
   cacheDir: '../../../node_modules/.vite/libs/series/marvel-studios',
-
+  plugins: [
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(import.meta.dirname, 'tsconfig.lib.json'),
+      pathsToAliases: false,
+    }),
+  ],
+  build: {
+    outDir: '../../../dist/libs/series/marvel-studios',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'series-marvel-studios',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es' as const],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: [],
+    },
+    assetsInlineLimit: 100000,
+  },
   test: {
+    globals: true,
     coverage: {
       reportsDirectory: '../../../coverage/libs/series/marvel-studios',
     },
