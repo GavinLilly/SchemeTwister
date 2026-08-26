7f6ac68cb1258a29dcb5e8db84026e41e3e024c8
Gavin Lilly <gavtherav@gmail.com>
2026-08-26 10:58:08 +0100

    build(*): add devenv config

---
+++ devenv.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/languages/
+  languages = {
+    javascript = {
+      enable = true;
+      package = pkgs.nodejs_22;
+    };
+    typescript.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
