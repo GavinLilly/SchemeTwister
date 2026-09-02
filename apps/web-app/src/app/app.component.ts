8121ff3069e14be16973be727b626ee5a45e25c4
Gavin Lilly <gavtherav@gmail.com>
2026-09-02 20:57:52 +0100

    fix(app): remove useless app imports

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,17 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent, HeaderComponent } from '@schemetwister/web-app/ui';
-
-import { UpdateService } from './update.service';
 
 @Component({
   selector: 'schemetwister-root',
   templateUrl: './app.component.html',
   imports: [RouterOutlet, FooterComponent, HeaderComponent],
 })
-export class AppComponent {
-  private readonly _sw = inject(UpdateService);
-  firestore: Firestore = inject(Firestore);
-}
+export class AppComponent {}
