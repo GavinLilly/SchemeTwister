60cbd107af26cc903d9a7d4b277bf4ab5e4870d3
Gavin Lilly <gavtherav@gmail.com>
2026-09-04 00:22:52 +0100

    build(*): add devenv devcontainer

--- devenv.nix
+++ devenv.nix
@@ -5,15 +5,36 @@
   ...
 }:
 {
-  # https://devenv.sh/languages/
   languages = {
     javascript = {
       enable = true;
-      package = pkgs.nodejs_22;
+      package = pkgs.nodejs-slim_22;
       pnpm.enable = true;
     };
     typescript.enable = true;
+
+    java.enable = true;
   };
 
-  # See full reference at https://devenv.sh/reference/options/
+  devcontainer = {
+    enable = true;
+    settings = {
+      customizations.vscode = {
+        extensions = [
+          "ms-vscode-remote.remote-containers"
+          "sonarsource.sonarlint-vscode"
+          "nrwl.angular-console"
+          "angular.ng-template"
+          "esbenp.prettier-vscode"
+          "knisterpeter.vscode-commitizen"
+          "firsttris.vscode-jest-runner"
+          "dbaeumer.vscode-eslint"
+        ];
+
+        settings = {
+          "sonarlint.pathToNodeExecutable" = "${config.languages.javascript.package}/bin/node";
+        };
+      };
+    };
+  };
 }
